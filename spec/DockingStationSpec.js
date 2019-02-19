5431b0410f0bc7a9c809d7f95b75fe9f7d0c4d56
Asad Khan <asad_khan47@hotmail.co.uk>
2019-02-19 17:02:04 +0000

    Improved tests and code to stop instantiation of a new bike upon release bike

--- spec/DockingStationSpec.js
+++ spec/DockingStationSpec.js
@@ -3,7 +3,6 @@
 describe('Docking Station', function() {
   let dockingstation;
   let bike;
-  let bike1;
 
   beforeEach(function() {
     dockingstation = new DockingStation();
@@ -14,22 +13,39 @@ describe('Docking Station', function() {
     };
   });
 
-  describe('.releaseBike(bike)', function() {
+  describe('.dockBike(bike)', function() {
     it('can dock a bike', function() {
+      expect(dockingstation.dockBike(bike)).toEqual(bike);
+    });
+  });
+
+  describe('.releaseBike(bike)', function() {
+    it('prevents a bike release if storage is empty', function() {
+      expect(function() { dockingstation.releaseBike(bike); }).toThrowError('bike storage is empty')
+    })
+
+    it('can release a bike', function() {
+      dockingstation.dockBike(bike)
       expect(dockingstation.releaseBike(bike)).toEqual(bike);
     });
 
     it('can check if a released bike is working', function() {
+      dockingstation.dockBike(bike)
       bike = dockingstation.releaseBike(bike);
       expect(bike.checkWorking()).toEqual(true);
     });
   });
 
-  describe('.dockBike(bike)', function() {
-    it('can release a bike', function() {
-      bike1 = dockingstation.releaseBike(bike);
-      dockingstation.dockBike(bike1);
-      expect(dockingstation.checkStorage()).toEqual([bike1]);
+  describe('.checkStorage', function() {
+    it('can check if a bike is stored after docking a bike', function() {
+      dockingstation.dockBike(bike)
+      expect(dockingstation.checkStorage()).toEqual([bike]);
     });
-  });
+
+    it('can check the bikes storage once the bike has been released: should be empty', function() {
+      dockingstation.dockBike(bike);
+      dockingstation.releaseBike(bike);
+      expect(dockingstation.checkStorage()).toEqual([]);
+    })
+  })
 });
--- src/DockingStation.js
+++ src/DockingStation.js
@@ -4,13 +4,18 @@ function DockingStation() {
   this.storedBikes = [];
 }
 
-DockingStation.prototype.releaseBike = function(bike = new Bike()) {
-  return bike;
-};
-
 DockingStation.prototype.dockBike = function(bike) {
   this.storedBikes.push(bike);
+  return bike;
 }
+
+DockingStation.prototype.releaseBike = function(bike) {
+  if(this.storedBikes.length === 0) {
+    throw new Error('bike storage is empty')
+  }
+  this.storedBikes.pop();
+  return bike;
+};
 
 DockingStation.prototype.checkStorage = function() {
   return this.storedBikes;

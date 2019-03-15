27b69bdd4985acef5f9bbc7457135166dded4ad2
Asad Khan <asad_khan47@hotmail.co.uk>
2019-03-15 11:58:51 +0000

    Improved test readability which incorporates working and broken bikes rather than bike and bike2

--- spec/DockingStationSpec.js
+++ spec/DockingStationSpec.js
@@ -2,17 +2,18 @@
 
 describe('Docking Station', function() {
   let dockingstation;
-  let bike;
-  let bike2;
+  let workingBike;
+  let brokenBike;
+  let workingBikeX;
 
   beforeEach(function() {
     dockingstation = new DockingStation();
-    bike = {
+    workingBike = {
       checkStatus: function() {
         return true;
       }
     };
-    bike2 = {
+    brokenBike = {
       checkStatus: function() {
         return false;
       }
@@ -23,52 +24,52 @@ describe('Docking Station', function() {
     var times = 5;
     var i = 0;
     for(i; i < times; i++) {
-      dockingstation.dockBike(bike);
+      dockingstation.dockBike(workingBike);
     }
   }
 
   describe('.dockBike(bike)', function() {
     it('can dock a bike', function() {
-      expect(dockingstation.dockBike(bike)).toEqual(bike);
+      expect(dockingstation.dockBike(workingBike)).toEqual(workingBike);
     });
 
     it('raises an error if the dockingstation cannot accept more bikes', function() {
-      defaultCapacity(bike);
-      expect(function() { dockingstation.dockBike(bike); }).toThrowError('bike storage is full')
+      defaultCapacity(workingBike);
+      expect(function() { dockingstation.dockBike(workingBike); }).toThrowError('bike storage is full')
     });
   });
 
   describe('.releaseBike(bike)', function() {
     it('prevents a bike release if storage is empty', function() {
-      expect(function() { dockingstation.releaseBike(bike); }).toThrowError('bike storage is empty')
+      expect(function() { dockingstation.releaseBike(workingBike); }).toThrowError('bike storage is empty')
     })
 
     it('can release a bike', function() {
-      dockingstation.dockBike(bike)
-      expect(dockingstation.releaseBike(bike)).toEqual(bike);
+      dockingstation.dockBike(workingBike)
+      expect(dockingstation.releaseBike(workingBike)).toEqual(workingBike);
     });
 
     it('can check if a released bike is working', function() {
-      dockingstation.dockBike(bike)
-      bike = dockingstation.releaseBike(bike);
-      expect(bike.checkStatus()).toEqual(true);
-    }); // check this in a second
+      dockingstation.dockBike(workingBike)
+      workingBikeX = dockingstation.releaseBike(workingBike);
+      expect(workingBikeX.checkStatus()).toEqual(true);
+    });
 
     it('fails to release a broken bike', function() {
-      dockingstation.dockBike(bike2)
-      expect(function() { dockingstation.releaseBike(bike2); }).toThrowError('sorry, this bike is broken')
+      dockingstation.dockBike(brokenBike)
+      expect(function() { dockingstation.releaseBike(brokenBike); }).toThrowError('sorry, this bike is broken')
     });
   });
 
   describe('.checkStorage', function() {
     it('can check if a bike is stored after docking a bike', function() {
-      dockingstation.dockBike(bike)
-      expect(dockingstation.checkStorage()).toEqual([bike]);
+      dockingstation.dockBike(workingBike)
+      expect(dockingstation.checkStorage()).toEqual([workingBike]);
     });
 
     it('can check the bikes storage once the bike has been released: should be empty', function() {
-      dockingstation.dockBike(bike);
-      dockingstation.releaseBike(bike);
+      dockingstation.dockBike(workingBike);
+      dockingstation.releaseBike(workingBike);
       expect(dockingstation.checkStorage()).toEqual([]);
     });
   });
